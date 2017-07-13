4776625ed5228d0c0219189b0a52db8bf367f1f6
devabhi <abhiachal.jain@gmail.com>
2017-07-13 19:52:41 +0530

    Create z2_plus.mk

---
+++ z2_plus.mk
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ROM>
+<RomName>AIM-ROM</RomName>
+<VersionName>AIM-System-V2.0_20170701-OFFICIAL-z2_plus</VersionName>
+<VersionNumber type="integer">20170701</VersionNumber>
+<DirectUrl><![CDATA[https://dc1.androidfilehost.com/dl/DfjcdazdJqrZI0WuQFAeCw/1500186014/889764386195912793/AIM-System-V2.0_20170701-OFFICIAL-z2_plus.zip]]></DirectUrl>
+<HttpUrl><![CDATA[https://www.androidfilehost.com/?fid=889764386195912793]]></HttpUrl>
+<Android>7.1.2</Android>
+<CheckMD5>a160bac3e223d2668ec2f06b01d08c51</CheckMD5>
+<FileSize type="integer">484337255</FileSize>
+<Developer><![CDATA[devabhi]]></Developer>
+<WebsiteURL><![CDATA[https://forum.xda-developers.com/lenovo-zuk-z2/development/rom-aim-rom-t3619474]]></WebsiteURL>
+<DonateURL><![CDATA[https://www.paypal.me/AIMroms]]></DonateURL>
+<Changelog>
+<![CDATA[
+#  AIMROM July Update :
+# Note : 
+_Clean flash is always recommended. Bug reports are not appreciated  if not clean flashed.
+Builds will be available soon when maintainers will get free time. So don't ask for ETAs._
+---
+]]>
+</Changelog>
+<AddonCount>3</AddonCount>
+<AddonsURL>
+https://goo.gl/KUWru6
+</AddonsURL>
+</ROM>
